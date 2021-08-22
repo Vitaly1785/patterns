fb9260a3c98e0bd86dfbba03aaedb1c40856c5f2
Vitaly1785 <korsar1785@yandex.ru>
2021-08-22 19:39:53 +0300

    cafeMenu added

--- src/IteratorMenu/CafeTestDrive.java
+++ src/IteratorMenu/CafeTestDrive.java
@@ -4,8 +4,9 @@ public class CafeTestDrive {
     public static void main(String[] args) {
         PancakeHouse pancakeHouse = new PancakeHouse();
         DinnerMenu dinnerMenu = new DinnerMenu();
+        CafeMenu cafeMenu = new CafeMenu();
 
-        Waitress waitress = new Waitress(pancakeHouse, dinnerMenu);
+        Waitress waitress = new Waitress(pancakeHouse, dinnerMenu, cafeMenu);
         waitress.printMenu();
     }
 }
--- src/IteratorMenu/Waitress.java
+++ src/IteratorMenu/Waitress.java
@@ -5,19 +5,24 @@ import java.util.Iterator;
 public class Waitress {
     private Menu pancakeHouse;
     private Menu dinnerMenu;
+    private Menu cafeMenu;
 
-    public Waitress(PancakeHouse pancakeHouse, DinnerMenu dinnerMenu) {
+    public Waitress(PancakeHouse pancakeHouse, DinnerMenu dinnerMenu, CafeMenu cafeMenu) {
         this.pancakeHouse = pancakeHouse;
         this.dinnerMenu = dinnerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu(){
         Iterator<MenuItem> pancakeIterator = pancakeHouse.createIterator();
         Iterator<MenuItem> dinnerIterator = dinnerMenu.createIterator();
+        Iterator<MenuItem> lunchIterator = cafeMenu.createIterator();
         System.out.println("MENU\\n----\\nBREAKFAST");
         printMenu(pancakeIterator);
         System.out.println("MENU\\n----\\nDINNER");
         printMenu(dinnerIterator);
+        System.out.println("MENU\\n----\\nLunch");
+        printMenu(lunchIterator);
     }
 
     private void printMenu(Iterator<MenuItem> iteratorMenu){
